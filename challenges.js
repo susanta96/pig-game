7c2d3ea2d279fc28c34934d541b31e0c1e2a04b0
susanta96 <susant.vanu7278@gmail.com>
2018-11-23 20:29:24 +0530

    working new user Scoring feature

--- challenges.js
+++ challenges.js
@@ -30,7 +30,7 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
             //player lose entire score
             scores[activePlayer] = 0;
             document.querySelector('#score-' + activePlayer).textContent = '0';
-            nextPlayer();
+            nextPlayer(); 
         }else if(dice !== 1){
             //Add score
             roundScore += dice;
@@ -54,8 +54,17 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         //Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        //Check is any input is there or not
+        if(input){
+            winningScore = input;
+        }else{
+            winningScore = 100;
+        }
+ 
         // Check the player won the game
-        if(scores[activePlayer] >= 100){
+        if(scores[activePlayer] >= winningScore){  
             document.querySelector('#name-' +activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
